8337bd68b86b990089032ef2e87c4cb97df1039e
Wiwyndra <wiwyndra@gmail.com>
2017-08-15 22:15:38 +0300

    Загрузка основных страниц

---
+++ scripts.js
@@ -0,0 +1,19 @@
+﻿function showHide(element_id) {
+                //Если элемент с id-шником element_id существует
+                if (document.getElementById(element_id)) { 
+                    //Записываем ссылку на элемент в переменную obj
+                    var obj = document.getElementById(element_id); 
+                    //Если css-свойство display не block, то: 
+                    if (obj.style.display != "block") { 
+                        obj.style.display = "block";//Показываем элемент
+						obj.scrollIntoView(false);						
+                    }
+                    else obj.style.display = "none"; //Скрываем элемент
+                }
+                //Если элемент с id-шником element_id не найден, то выводим сообщение
+                else alert("Извините, в данный момент этот раздел находится на редактировании!"); 
+				/* this.scrollIntoView(); */
+            }   
+			
+
+			
