4fd4a953577099a3ec8e9bc0d72900555b416fab
Wiwyndra <wiwyndra@gmail.com>
2018-01-19 14:51:06 +0300

    подгон до версии 2,0

--- scripts.js
+++ scripts.js
@@ -12,8 +12,30 @@
                 }
                 //Если элемент с id-шником element_id не найден, то выводим сообщение
                 else alert("Извините, в данный момент этот раздел находится на редактировании!"); 
-				/* this.scrollIntoView(); */
+				
             }   
 			
+function hashSearch(value) {
+	var Items = document.getElementById('hashFilter');// в диве hashFilter	
+	var items = Items.getElementsByClassName('news-item');// отбираем все элементы с классом news-item
+		for (var i=0; i<items.length; i++) {// для каждого в списке
+			if (items[i].classList.contains(value)) {//если в имени класса содержится искомое значение
+				items[i].style.display = 'block';//показать
+			} else {
+				items[i].style.display = 'none';//скрыть
+			}
+		}
+		
+	window.location = "#"+value;  //при активации функции добавить искомое значение через хэш к url
+	
+ //сброс кнопки "назад" браузера, "починка" нормального течения события "назад" - полное открытие списка новостей
+	window.addEventListener("popstate",function(e) { 	//прослушка события  "popstate", добавлена фукция обработки	
+			document.location.href = "news.html"; //переход на вкладку "news.html"
+			e.preventDefault(); //сброс исходного значения действия
+		},false); 
+	
+}
+
+
 
 			
